02736da7b2e190dfcbda741f81aa6560ecb8e666
René <luijk.r@gmail.com>
2020-01-24 20:08:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# News analyzer service
+
+Small service that calls the [TextRazor](https://www.textrazor.com/) API. This repos is part of a larger project fetching and analyzing news articles from several different outlets.
---
+++ src/service.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const request = require("request");
+
+const app = express();
+const router = express.Router();
+
+router.post("/analyze_url", (req, res) => {
+  const options = {
+    url: "https://api.textrazor.com",
+    headers: {"x-textrazor-key": req.headers["api-key"]}
+  };
+
+  request.post(options).form({
+    extractors: "topics",
+    classifiers: "textrazor_iab_content_taxonomy",
+    url: req.body.url
+  }).pipe(res);
+});
+
+app.use(express.json());
+app.use("/.netlify/functions/service", router);
+
+module.exports.handler = serverless(app);
